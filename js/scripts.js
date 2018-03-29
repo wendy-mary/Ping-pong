d14f77c29f80cd208d445c474728c613a32a435e
wendy mary <marywendychange@yahoo.com>
2018-03-29 13:09:19 +0300

    renamed js/jquery, modified index.html and scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,34 @@
-$(document).ready(function() {
-  var arr =[];
-  function test(number){
-    for (i = 1;)
-  }
 
-})
+var arr = [];
+function ping(init) {
+ for(var i=1; i<=init; i++){
+
+  if ((i % 3 === 0 && i % 5 === 0)) {
+    arr.push('pingpong');
+  } else if (i % 5 === 0) {
+    arr.push('pong');
+  } else if (i % 3 === 0) {
+    arr.push('ping');
+  } else {
+    arr.push(i);
+  }
+}
+
+};
+
+$(document).ready(function() {
+  $('#sub').click(function(event) {
+  event.preventDefault();
+    var userNumber = $('input#num').val();
+    ping(userNumber);
+    // console.log(arr);
+    // arr.forEach(fuction (c){
+    //
+    // });
+arr.forEach(function(c){
+  $("#list").append($("<li>").html(c));
+});
+
+
+  });
+});
