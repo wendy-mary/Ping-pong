a91a4f58391ba04d721b3e5bcc8f124c4f9abe60
wendy mary <marywendychange@yahoo.com>
2018-03-30 09:36:24 +0300

    added styles on id header and h4,modify scripts.js and index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,33 @@
-
 var arr = [];
-function ping(init) {
- for(var i=1; i<=init; i++){
 
-  if ((i % 3 === 0 && i % 5 === 0)) {
-    arr.push('pingpong');
-  } else if (i % 5 === 0) {
-    arr.push('pong');
-  } else if (i % 3 === 0) {
-    arr.push('ping');
-  } else {
-    arr.push(i);
+function ping(init) {
+  for (var i = 1; i <= init; i++) {
+
+    if ((i % 3 === 0 && i % 5 === 0)) {
+      arr.push('pingpong');
+    } else if (i % 5 === 0) {
+      arr.push('pong');
+    } else if (i % 3 === 0) {
+      arr.push('ping');
+    } else {
+      arr.push(i);
+    }
   }
-}
 
 };
 
 $(document).ready(function() {
   $('#sub').click(function(event) {
-  event.preventDefault();
+    event.preventDefault();
     var userNumber = $('input#num').val();
     ping(userNumber);
     // console.log(arr);
     // arr.forEach(fuction (c){
     //
     // });
-arr.forEach(function(c){
-  $("#list").append($("<li>").html(c));
-});
+    arr.forEach(function(c) {
+      $("#list").append($("<li>").html(c));
+    });
 
 
   });
